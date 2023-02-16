9c9d6113d8bd407b30a078da71dcd09762fb42a7
Pisen99 <patricia.andersson99@hotmail.com>
2023-02-16 23:52:34 +0000

    Snippets

---
+++ my-app/src/components/ES7snippets.js
@@ -0,0 +1,54 @@
+// /* rfce then press enter*/
+// import React from 'react'
+
+// function ES7snippets() {
+//   return (
+//     <div>ES7snippets</div>
+//   )
+// }
+
+// export default ES7snippets
+
+
+// /* rafce then press ente (arrowfunction)*/
+// import React from 'react'
+
+// const ES7snippets = () => {
+//   return (
+//     <div>ES7snippets</div>
+//   )
+// }
+
+// export default ES7snippets
+
+
+// /* rce then press ente (class component)*/
+// import React, { Component } from 'react'
+
+// export class ES7snippets extends Component {
+//   render() {
+//     return (
+//       <div>ES7snippets</div>
+//     )
+//   }
+// }
+
+// export default ES7snippets
+
+
+// /* rconst then press enter (state)*/
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//      first
+//   }
+// }
+
+
+// /* cgl to log something */
+// console.log(first)
+
+
+// /* cgl to log objects too */
+// console.log('first', first)
